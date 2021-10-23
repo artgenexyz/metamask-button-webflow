dfee4037abbad3a1d11c78ab7545b4a9dbcac5c9
Daniel Dhawan <shadowagent1st@gmail.com>
2021-10-23 19:55:15 +0300

    Fixed updateMetaMaskStatus

--- wallet.js
+++ wallet.js
@@ -29,7 +29,7 @@ export const connectMetaMask = async () => {
   }
 }
 
-document.onload = () => {
+window.onload = () => {
   updateMetaMaskStatus();
 }
 document.querySelector(window.buttonID ?? '#connect').addEventListener('click', connectMetaMask);
